c5530bf59d9fbc4be7b0ec134acc45cc99cf9258
mhkmhsh <mahekmahesh12@gmail.com>
2024-12-05 09:29:46 -0500

    save idk what

--- JavaProjects/point/LineSegment.java
+++ JavaProjects/point/LineSegment.java
@@ -1,8 +1,26 @@
 
 public class LineSegment {
 
-    private String labelPoint1;
-    private String labelPoint2;
-    private String point1;
-    private String Point2;
+    private String labelPoint1, labelPoint2;
+    private Point p1;
+    private Point p2;
+
+public LineSegment ( Point p1, Point p2, String sp1, String sp2){
+    this.p1 =p1;
+    this.p2=p2;
+    labelPoint1 = sp1;
+    labelPoint2 = sp2
+}
+public LineSegment ( Point pt1, Point pt2){
+    this(pt1, pt2,"A", "B")
+}
+public double LengthOfSegment(){
+    Double diffX = p1.getX() - p2.getX();
+    Double diffY = p1.getY() - p2.getY();
+    return Math.sqrt( diffX * diffX + diffY * diffY);
+
+}
+public String toString(){
+    return labelPoint1 + " " + p1 + " " + labelPoint2+ " " + p2;
+}
 }
