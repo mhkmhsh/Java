c7cd57950e344c3c87e29f8690e5f80977d70f0c
mhkmhsh <mahekmahesh12@gmail.com>
2025-02-28 11:26:29 -0500

    fegfsdgwe

--- unit-9/football/Bouncing Ball Lab/src/SizeChangingBall.java
+++ unit-9/football/Bouncing Ball Lab/src/SizeChangingBall.java
@@ -5,24 +5,24 @@ public class SizeChangingBall extends Ball{
     private int maxDiameter, minDiameter;
     private int diameterStep;
 
-    public sizeChangingBall(int xStart, int yStart, Color c, int) {
+    public sizeChangingBall(int xStart, int yStart, Color c, int maxD) {
         super(xStart, yStart, c);
         maxDiameter = maxD;
         minDiameter =2;
         diameterStep =1;
-
+    }
 
     public void changeDiameter() {
         int diam = getDiam();
-        dian += diameterStep;
+        diam += diameterStep;
         if (diam >= maxDiameter || diam <= minDiameter) {
             diameterStep = -diameterStep;
         }
         setDiam(diam);
     }
  public void move(int width, int height){
-    Superclass.move(width,height);
+    super.move(width,height);
     changeDiameter();
  }
 }
-}
+
