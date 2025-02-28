2252d2427a5e497452d94cba7efa053e24439ae0
mhkmhsh <mahekmahesh12@gmail.com>
2025-02-28 11:49:45 -0500

    save

--- unit-9/football/Bouncing Ball Lab/src/BouncingBalls.java
+++ unit-9/football/Bouncing Ball Lab/src/BouncingBalls.java
@@ -10,10 +10,11 @@ public class BouncingBalls {
     {
         width = w;
         height = h;
-        balls = new Ball[3];
+        balls = new Ball[4];
         balls[0] = new Ball(100,50,Color.BLUE);
-        balls[1] = new ColorfulBall(10,10,10,-5,8);
+        balls[1] = new ColorfulBall(10,10,20,-5,8);
         balls[2] = new SizeChangingBall(200,200,Color.CYAN, 50);
+        balls[3] = new Ball(10,10,Color.GREEN);
       
     }
 
