c0643c5ae4f3830fe701e7fa4a211ca0bd3dbac4
mhkmhsh <mahekmahesh12@gmail.com>
2025-03-07 08:41:04 -0500

    idk

--- unit-9/football/Bouncing Ball Lab/src/SoundBall.java
+++ unit-9/football/Bouncing Ball Lab/src/SoundBall.java
@@ -1,19 +1,32 @@
 
 import java.awt.*;
 
-
 public class SoundBall extends Ball {
 
+    private int xLoc, yLoc, diameter;
+    private int xStep, yStep;
+    public SoundBall
+
+    {
+        xLoc = x;
+        yLoc = y;
+        xStep = xMove;
+        yStep = yMove;
+
+    }
+
     public void move(int width, int height) {
+
+        int radius = diameter / 2;
         if (Math.abs(xLoc + xStep) + radius > width) {
             xStep = -xStep;
             StdAudio.play("car.wav");
         }
-        if (Math.abs(yLoc + yStep) + radius > height)  {
+        if (Math.abs(yLoc + yStep) + radius > height) {
             yStep = -yStep;
             StdAudio.play("car.wav");
         }
-        
+
     }
 
 }
