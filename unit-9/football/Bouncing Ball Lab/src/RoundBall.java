ce3edf36984e5938fc03541952217edc9b986317
mhkmhsh <mmahesh12@gmail.com>
2025-03-10 11:33:44 -0400

    save

--- unit-9/football/Bouncing Ball Lab/src/RoundBall.java
+++ unit-9/football/Bouncing Ball Lab/src/RoundBall.java
@@ -1,41 +1,25 @@
-
-import java.awt.*;
+import java.awt.Color;
 
 public class RoundBall extends Ball {
 
-    private int x, y; 
-    private int centerX, centerY; 
+    private int centerX, centerY;
     private int radius;
-    private double angle = 0; 
-    private Timer timer;
+    private double angle = 0;
 
-
-    public RoundBall(int centerX, int centerY, int radius) {
+    public RoundBall(int centerX, int centerY, Color c, int radius) {
+        super(centerX + radius, centerY, 10, 0, 0, c);
         this.centerX = centerX;
         this.centerY = centerY;
         this.radius = radius;
-        super(c);
-        StdDraw.fillOval(x - 10, y - 10, 20, 20);
-        StdDraw.setColor(Color.RED);
-        x = (int) (centerX + radius);
-        y = centerY;
-        timer = new Timer(20, this);
-        timer.start();
     }
 
-    public void move(int width, int height)
-    {
-        angle += 0.02;
+    public void move(int width, int height) {
+        angle += 0.05;
         if (angle > 2 * Math.PI) {
             angle -= 2 * Math.PI;
         }
 
-        x = (int) (centerX + radius * Math.cos(angle));
-        y = (int) (centerY + radius * Math.sin(angle));
-
-     
-        
-        
+        setxLoc((int) (centerX + radius * Math.cos(angle)));
+        setyLoc((int) (centerY + radius * Math.sin(angle)));
     }
-
 }
