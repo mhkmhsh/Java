5cddd78cf81d76edef46ef5993472e7cf759e814
mhkmhsh <mahekmahesh12@gmail.com>
2025-03-10 09:57:13 -0400

    great

--- unit-9/football/Bouncing Ball Lab/src/RoundBall.java
+++ unit-9/football/Bouncing Ball Lab/src/RoundBall.java
@@ -3,34 +3,30 @@ import java.awt.*;
 
 public class RoundBall extends Ball {
 
-    private double x0, y0, x1, y1;
-    private double ballRadius;
-
-    public LineWithBall(double x0, double y0, double x1, double y1, double ballRadius) {
-        this.x0 = x0;
-        this.y0 = y0;
-        this.x1 = x1;
-        this.y1 = y1;
-        this.ballRadius = ballRadius;
-    }
+   
+   private int radius;
 
     public RoundBall(int x, int y, Color c, int radius) {
         super(x, y, c);
         this.radius = radius;
-    }
-
-    public void drawBall() {
-        StdDraw.line(new Line2D.Double(x0, y0, x1, y1));;
-        StdDraw.setPenColor(getColor());
-        StdDraw.fillOval((int) (x0 - ballRadius), (int) (y0 - ballRadius), (int) (2 * ballRadius), (int) (2 * ballRadius));;
-
+        
     }
 
    
 
-    public void move(int width, int height) {
-        LineWithBall line = new LineWithBall(100, 50, 200, 150, 20);
+    public void move(int width, int height)
+    {
+        int radius = diameter/2;
 
+  
+        if (Math.abs(xLoc + xStep) + radius > width) {
+            xStep = -xStep;
+            StdAudio.play("pong.wav");
+        }
+        
+        xLoc += xStep; 
+        
+        
     }
 
 }
