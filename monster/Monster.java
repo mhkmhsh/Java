07044ca1ae4930b25ba31beffff1ddfd31fd789c
mhkmhsh <mahekmahesh12@gmail.com>
2025-04-03 10:35:13 -0400

    commit idk

---
+++ monster/Monster.java
@@ -0,0 +1,39 @@
+
+public class Monster {
+
+    private String name;
+    private int health;
+    private int attackPower;
+    private static int totalMonsters;
+
+    public Monster(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+        totalMonsters++;
+    }
+
+    public void attack(Monster otherMonster) {
+        return;
+    }
+
+    public Boolean isDefeated() {
+        if (health <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return;
+    }
+
+    public static int getTotalMonsters() {
+        return totalMonsters;
+    }
+
+    public Player() {
+
+    }
+}
