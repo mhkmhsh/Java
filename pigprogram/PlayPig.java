57266ff510ee16bdf51deefb981d4543e049e773
mhkmhsh <mahekmahesh12@gmail.com>
2025-06-06 11:22:09 -0400

    saving

---
+++ pigprogram/PlayPig.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class PlayPig {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String p1 = inp.nextLine();
+        String p2 = inp.nextLine();
+    }
+    public static void welcome(){
+        System.out.println("Welcome to the game, roll to start");
+    }
+    public int dice(int roll){
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ pigprogram/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private String name;
+    private int totalScore;
+    private int turnScore;
+
+   public Player(String name){
+
+    }
+  
+
+}
