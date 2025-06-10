c61a9246e8ff63b70fca5aeba6ab6e044d1e38c4
mhkmhsh <mahekmahesh12@gmail.com>
2025-06-10 08:51:57 -0400

    extending it idk if needed yet

--- pigprogram/PlayPig.java
+++ pigprogram/PlayPig.java
@@ -10,7 +10,16 @@ public class PlayPig {
     public static void welcome(){
         System.out.println("Welcome to the game, roll to start");
     }
-   
+   public int firstRoll(){ 
+    boolean p1Turn = true; 
+    if ( p1Turn = true){
+        int roll = (int)(Math.random() * 6) + 1;
+         p1Turn = false;
+    }
+    else {
+         
+    }
+   }
 
 
 
--- pigprogram/Player.java
+++ pigprogram/Player.java
@@ -1,15 +1,32 @@
-public class Player {
+import java.util.Scanner;
+
+public class Player extends PlayPig {
     private String name;
     private int totalScore;
-    private int turnScore;
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return totalScore;
+    }
 
    public Player(String name){
         this.name = name;
-        this.totalScore = totalScore;
-        this.turnScore = turnScore;
-        
-
+        this.totalScore = 0;
     }
-  
-
+    public void takeTurn(){
+         Scanner one = new Scanner(System.in);
+         System.out.println("Type any letter to roll the dice: ");
+         one.nextLine();
+         /* gen a random number, 
+         tell user current round total
+         prompt roll again or no
+        get out if roll 1 or say no */
+        int currRoundTot = 0;
+        while( true ){
+            if(roll!= 1){
+                currRoundTot = roll + currRoundTot;
+            }
+        }
+    }
 }
